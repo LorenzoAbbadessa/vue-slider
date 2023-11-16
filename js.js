f331eab83ecfae65d5e01315a0b042a98db842d0
lorenzo <lor.abbadessa@gmail.com>
2023-11-16 16:24:51 +0100

    continua esercizio

---
+++ js.js
@@ -0,0 +1,52 @@
+const { createApp } = Vue
+
+createApp({
+    data() {
+        items = [
+            {
+                immagine: 'https://img.freepik.com/free-photo/sunset-time-tropical-beach-sea-with-coconut-palm-tree_74190-1075.jpg',
+                titolo: 'Tramonto sulla spiaggia',
+                descrizione: "Un meraviglioso tramonto con colori caldi che si riflettono sull'acqua."
+            },
+            {
+                immagine: 'https://img.freepik.com/free-photo/beautiful-shot-mountains-trees-covered-snow-fog_181624-17590.jpg',
+                titolo: 'Montagne innevate',
+                descrizione: 'Un panorama mozzafiato delle montagne coperte di neve.'
+            },
+            {
+                immagine: 'https://img.freepik.com/free-photo/countryside-field-sunny-day-countryside_181624-24010.jpg',
+                titolo: 'Campi verdi',
+                descrizione: "Vaste distese di campi verdi che si estendono all'orizzonte."
+            }
+        ]
+    }
+    }).mount('#app');
+
+
+
+/*
+createApp({
+    data() {
+        slides = [
+            {
+                immagine: 'https://img.freepik.com/free-photo/sunset-time-tropical-beach-sea-with-coconut-palm-tree_74190-1075.jpg',
+                titolo: 'Tramonto sulla spiaggia',
+                descrizione: "Un meraviglioso tramonto con colori caldi che si riflettono sull'acqua."
+            },
+            {
+                immagine: 'https://img.freepik.com/free-photo/beautiful-shot-mountains-trees-covered-snow-fog_181624-17590.jpg',
+                titolo: 'Montagne innevate',
+                descrizione: 'Un panorama mozzafiato delle montagne coperte di neve.'
+            },
+            {
+                immagine: 'https://img.freepik.com/free-photo/countryside-field-sunny-day-countryside_181624-24010.jpg',
+                titolo: 'Campi verdi',
+                descrizione: "Vaste distese di campi verdi che si estendono all'orizzonte."
+            }
+        ]
+    }
+        }).mount('#app');
+
+
+*/
+        
